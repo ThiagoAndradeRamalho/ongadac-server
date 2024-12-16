dfe77e7500ac3a30b6adf218038feadfd57302b0
Thiago Andrade <thiandrade79@gmail.com>
2024-12-16 16:44:31 -0300

    add bd

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
+
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Algo deu errado!');
--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 import 'dotenv/config';
 import app from './app.js';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+const mongoose = require('mongoose');
+
+const DATABASE_URL = "mysql://root:eNKkOLZkEHfQVVtZNnDezPAPnkAbHwPM@autorack.proxy.rlwy.net:30437/railway";
+
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log("Conectado ao banco de dados com sucesso!");
+    mongoose.connection.close();
+  })
+  .catch(error => {
+    console.error("Erro ao conectar no banco de dados:", error.message);
+  });
+
+  mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then(() => console.log("Banco de dados conectado com sucesso"))
+    .catch(error => {
+      console.error("Erro ao conectar no banco de dados:", error.message);
+      process.exit(1); // Encerra o processo caso não consiga conectar
+    });
