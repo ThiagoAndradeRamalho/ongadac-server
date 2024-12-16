99cfeb2d22f4b7f5722210d0e413e133c44b9d8d
Thiago Andrade <thiandrade79@gmail.com>
2024-12-16 17:07:00 -0300

    Alterando de mongo para mysql

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import mysql from 'mysql2';
 import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
@@ -7,28 +8,21 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
-const mongoose = require('mongoose');
 
-const DATABASE_URL = "mysql://root:eNKkOLZkEHfQVVtZNnDezPAPnkAbHwPM@autorack.proxy.rlwy.net:30437/railway";
 
-mongoose.connect(DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => {
-    console.log("Conectado ao banco de dados com sucesso!");
-    mongoose.connection.close();
-  })
-  .catch(error => {
-    console.error("Erro ao conectar no banco de dados:", error.message);
-  });
+const connection = mysql.createConnection({
+  host: 'autorack.proxy.rlwy.net',
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: 30437
+});
 
-  mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-    .then(() => console.log("Banco de dados conectado com sucesso"))
-    .catch(error => {
-      console.error("Erro ao conectar no banco de dados:", error.message);
-      process.exit(1); // Encerra o processo caso não consiga conectar
-    });
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err.message);
+    process.exit(1); // Encerra o servidor em caso de erro na conexão
+  } else {
+    console.log('Conexão com o MySQL realizada com sucesso!');
+  }
+});
