50a20f41b0ea2298dee27826f3acc7f207d70497
Thiago Andrade <thiandrade79@gmail.com>
2024-12-16 17:45:44 -0300

    thiago

--- app.js
+++ app.js
@@ -15,23 +15,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Conectando ao banco de dados MySQL
+console.log('Tentando conectar ao banco de dados...');
 const connection = mysql.createConnection({
-  host: 'autorack.proxy.rlwy.net',
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: 30437,
-  connectTimeout: 10000,
+host: 'autorack.proxy.rlwy.net',
+user: process.env.DATABASE_USER,
+password: process.env.DATABASE_PASSWORD,
+database: process.env.MYSQL_DATABASE,
+port: 30437,
+connectTimeout: 10000,
 });
 
 // Testando a conexão com o banco de dados
 connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-    process.exit(1); // Encerrar a aplicação caso não consiga conectar ao banco
-  } else {
-    console.log('Conectado ao banco de dados');
-  }
+if (err) {
+  console.error('Erro ao conectar ao banco de dados:', err);
+  process.exit(1); // Encerrar a aplicação caso não consiga conectar ao banco
+} else {
+  console.log('Conectado ao banco de dados');
+}
 });
 
 // Definindo rotas
@@ -39,20 +40,22 @@ app.use('/api', routes);
 
 // Rota para consultar tarefas
 app.get('/api/tarefa', (req, res) => {
-  connection.query('SELECT * FROM tarefas', (err, results) => {
-    if (err) {
-      console.error('Erro ao consultar tarefas:', err);
-      res.status(500).json({ message: 'Erro ao consultar tarefas.' });
-    } else {
-      res.status(200).json(results);
-    }
-  });
+console.log('Consultando tarefas no banco de dados...');
+connection.query('SELECT * FROM tarefas', (err, results) => {
+  if (err) {
+    console.error('Erro ao consultar tarefas:', err);
+    res.status(500).json({ message: 'Erro ao consultar tarefas.' });
+  } else {
+    console.log('Consulta realizada com sucesso:', results);
+    res.status(200).json(results);
+  }
+});
 });
 
 // Tratamento de erro
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Algo deu errado!');
+console.error('Erro interno:', err.stack);
+res.status(500).send('Algo deu errado!');
 });
 
 export default app;
